2a2d460850150280206cc0004f0e20e8165896f1
Prashanthshah <shahprashanth4@gmai.com>
2022-01-06 23:01:16 +0530

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head';
-import Switches from '../components/switches';
+import Head from "next/head";
+import Switches from "../components/Switches";
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-mainbg'>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-mainbg">
       <Head>
         <title>ESP8266 NodeMCU - BMS</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Switches />
     </div>
